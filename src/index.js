a3968813d7c3cd1d480636924ef9b36d677903b7
Tito Araujo <titoraujo23@gmail.com>
2022-03-14 09:09:33 -0300

    Ending chaalleng 01 - nodejs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
-const res = require("express/lib/response");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const users = [
-  // {
-  //   "id": "b8817aac-fd6f-40d9-acaa-5f0fc146cd4c",
-  //   "name": "Vitor Araujo",
-  //   "username": "tito",
-  //   "todos": []
-  // }
-];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
@@ -84,54 +76,52 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   const { user } = request;
   const { id } = request.params;
 
-  const taskById = user.todos.find((task) => String(task.id) === String(id));
+  const task = user.todos.find((task) => String(task.id) === String(id));
 
-  if (!taskById) {
+  if (!task) {
     return response.status(404).json({
       error: "Task not found!",
     });
   }
 
-  taskById.title = title;
-  taskById.deadline = new Date(deadline);
+  task.title = title;
+  task.deadline = new Date(deadline);
 
-  return response.status(201).json(taskById);
+  return response.status(201).json(task);
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  const {id} = request.params
-  const {user} = request
-  const {done} = request.body
+  const { id } = request.params;
+  const { user } = request;
 
+  const task = user.todos.find((task) => String(task.id) === String(id));
 
-  const taskById = user.todos.find((task) => String(task.id) === String(id));
-
-  if (!taskById) {
+  if (!task) {
     return response.status(404).json({
       error: "Task not found!",
     });
   }
 
-  taskById.done = done
+  task.done = true;
 
-  return response.status(201).json(taskById)
-
+  return response.status(201).json(task);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  const {user} = request
-  const {id} = request.params
+  const { user } = request;
+  const { id } = request.params;
 
-  const indexTask = user.todos.findIndex(task => task.id === id)
+  const indexTask = user.todos.findIndex((task) => task.id === id);
 
-  if(indexTask <= -1 ){
+  if (indexTask === -1) {
     return response.status(404).json({
-      error:"Task not found"
-    })
+      error: "Task not found",
+    });
   }
-  user.todos.splice(indexTask,1)
 
-  return response.status(204)
+  user.todos.splice(indexTask, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
